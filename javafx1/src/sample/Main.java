eaf613560840cfcded019f4468afcde503bfb028
DaniiarR <danraiymkulov@mail.com>
2019-10-02 12:02:23 +0600

    final commit

--- javafx1/src/sample/Main.java
+++ javafx1/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("daniiar.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("BMI Calculator");
         primaryStage.setScene(new Scene(root, 900, 400));
         primaryStage.show();
     }
