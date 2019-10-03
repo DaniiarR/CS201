43dc92a865f77593f8aac8da9253e1c3e1fab5e4
DaniiarR <danraiymkulov@mail.com>
2019-10-03 21:51:26 +0600

    created the painter app

--- Painter/src/sample/Controller.java
+++ Painter/src/sample/Controller.java
@@ -1,15 +1,16 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
 
@@ -61,8 +62,40 @@ public class Controller {
     @FXML
     private Pane drawingAreaPane;
 
+    @FXML
+    private Slider redSlider;
+
+    @FXML
+    private Slider greenSlider;
+
+    @FXML
+    private Slider blueSlider;
+
+    @FXML
+    private Slider alphaSlider;
+
+    @FXML
+    private TextField redTextField;
+
+    @FXML
+    private TextField greenTextField;
+
+    @FXML
+    private TextField blueTextField;
+
+    @FXML
+    private TextField alphaTextField;
+
+    @FXML
+    private Rectangle colorRectangle;
+
     private PenSize radius = PenSize.MEDIUM;
     private Paint brushColor = Color.BLACK;
+
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
+    private double alpha = 1.0;
 
     public void initialize() {
         blackRadioButton.setUserData(Color.BLACK);
@@ -73,6 +106,32 @@ public class Controller {
         smallRadioButton.setUserData(PenSize.SMALL);
         mediumRadioButton.setUserData(PenSize.MEDIUM);
         largeRadioButton.setUserData(PenSize.LARGE);
+
+        redTextField.textProperty().bind(redSlider.valueProperty().asString("%.0f"));
+        greenTextField.textProperty().bind(greenSlider.valueProperty().asString("%.0f"));
+        blueTextField.textProperty().bind(blueSlider.valueProperty().asString("%.0f"));
+        alphaTextField.textProperty().bind(alphaSlider.valueProperty().asString("%.0f"));
+
+        redSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            red = t1.intValue();
+            colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+            brushColor = colorRectangle.getFill();
+        });
+        greenSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            green = t1.intValue();
+            colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+            brushColor = colorRectangle.getFill();
+        });
+        blueSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            blue = t1.intValue();
+            colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+            brushColor = colorRectangle.getFill();
+        });
+        alphaSlider.valueProperty().addListener((observableValue, number, t1) -> {
+            alpha = t1.intValue();
+            colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
+            brushColor = colorRectangle.getFill();
+        });
     }
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -86,8 +145,10 @@ public class Controller {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle circle = new Circle(event.getX(), event.getY(), radius.getRadius(), brushColor);
-        drawingAreaPane.getChildren().add(circle);
+        if (event.getX() > 0 && event.getY() > 0) {
+            Circle circle = new Circle(event.getX(), event.getY(), radius.getRadius(), brushColor);
+            drawingAreaPane.getChildren().add(circle);
+        }
     }
 
     @FXML
